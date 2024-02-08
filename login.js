c2a9641a2d56017813c55a33842a703c29d44e3f
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-02-08 11:53:58 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+const element = <Welcome name="Sara" />;
+
+export default function Login(props){
+    return(
+        <View style={styles.login}>
+            <Text>This is the Login Screen</Text>
+            <Button title="Log In" onPress={()=>{
+                console.log("Hello Team Scrum 7")
+            }}></Button>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'green',
+        height: '12%',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        
+      },
+})
