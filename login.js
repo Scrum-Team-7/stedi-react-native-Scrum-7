e636580330649665d2e2513bda5ad9ceb0320f27
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-02-15 11:52:33 -0700

    Update login.js
    Updated with names

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ export default function Login(props){
         <View style={styles.login}>
             <Text>This is the Login Screen</Text>
             <Button title="Log In" onPress={()=>{
-                console.log("Hello Team Scrum 7: Jesse")
+                console.log("Hello Team Scrum 7: Jesse Logan Makai")
             }}></Button>
         </View>
 
